e30f6ba7f18949e5ce2b90c380122f520a9b54ef
Chayapol Robkob <chayapol_gus@icloud.com>
2023-01-27 02:46:20 +0700

    4th commit

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require("express", () => {
 console.log("Successful imported express module");
 });
+const bp = require("body-parser");
+
+
+
+
 console.log("Hello world");
 console.log("Hello, World");
 
-const app = epress();
+const app = express();
+
+app.use(bp.urlencoded({extended: true}));
 
 app.get("/", (req,res) => {
-res.sendFile("index.html");
-})
+res.sendFile(__dirname+"/index.html");
+});
+
+
+app.post("/",(req,res) => {
+res.sendFile(__dirname+"/res.html");
+});
+
+
+
+app.post("/back", (req,res) => {
+	console.log("going back");
+res.sendFile(__dirname+"/index.html");
+});
+
+
+app.listen("3000");
