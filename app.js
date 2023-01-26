14a29d3e6f22193af90a71514e07b7f4c37a8a72
Chayapol Robkob <chayapol_gus@icloud.com>
2023-01-27 02:15:13 +0700

    3ed commit

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ console.log("Successful imported express module");
 });
 console.log("Hello world");
 console.log("Hello, World");
+
+const app = epress();
+
+app.get("/", (req,res) => {
+res.sendFile("index.html");
+})
